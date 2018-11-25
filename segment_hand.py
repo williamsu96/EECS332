ce0acd1ae9ebf482927907253e94c1e1e869e741
Tim Huang <tim81517lh@gmail.com>
2018-11-24 22:59:17 -0600

    second commit

--- segment_hand.py
+++ segment_hand.py
@@ -8,6 +8,7 @@ def segment_hand():
     # plt.switch_backend('QT4agg')
 
     img = cv2.imread('finger.png')
+    img_out = img[:]
     img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     img_hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
     # # Displaying original image
@@ -20,17 +21,21 @@ def segment_hand():
     cols = img.shape[1]
     box_rows = int(rows / 10 / 2)
     box_cols = int(cols / 10 / 2)
-    rect_centers = numpy.array([[cols / 3, rows / 3], [cols / 3, rows * 2 / 3], [cols * 2 / 3, rows / 3], [cols * 2 / 3, rows * 2 / 3]])
+    rect_centers = numpy.array([[rows / 3, cols / 3], [rows * 2 / 3, cols / 3], [rows / 3, cols * 2 / 3], [rows * 2 / 3, cols * 2 / 3]])
     sub_img = [0] * 4;
     for i in range(0, 4):
         x = int(numpy.round(rect_centers[i, 0]))
         y = int(numpy.round(rect_centers[i, 1]))
         sub_img[i] = img_hsv[(y - box_rows):(y + box_rows), (x - box_cols):(x + box_cols), :]
+        cv2.rectangle(img_out, (y - box_rows, x - box_cols), (y + box_rows, x + box_cols), (255, 0, 0), 1)
+    cv2.imwrite("finger_sq_overlay.png", img_out)
 
+    hist_ = cv2.calcHist([sub_img[0], sub_img[1], sub_img[2], sub_img[3]], [1, 2], None, [10,10], [0, 256, 0, 256])
     hist = cv2.calcHist([img_hsv], [0, 1], None, [50, 50], [0, 256, 0, 256])
     # cv2.imshow('figure: Histogram', hist)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
+
 
     plt.imshow(hist, interpolation='nearest')
     plt.savefig('histogram.png')
