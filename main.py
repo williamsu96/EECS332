7f010e073921c39eeece605a16b3a9ab2e9d1e88
williamsu96 <williamsu96@gmail.com>
2018-12-02 16:09:18 -0600

    drawing points functionality

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import segment_hand
 import generate_histogram
+import process_seg_image
 import cv2
 
 if __name__ == "__main__":
     # demo: after aligning your hand with the rectangles, press space to capture + generate histogram and then press
     # any key to close picture
     histogram = generate_histogram.generate_histogram_from_webcam()
-
+    draw_points = []
+    draw_length = 100
+    point_radius = 10
     webcam = cv2.VideoCapture(0)
     while True:
         ret_val, img = webcam.read()
 
         segmented_img = segment_hand.segment_hand(histogram,
-                                                  img)  # TODO: takes histogram and current webcam img and returns segmented output
+                                                  img)
 
         # TODO: function that identifies the finger tip as a cartesian coordinate
-
-        # TODO: function that stores fingertip positions and draws each one onto img
-
-        # cv2.imshow('webcam', img)
-        cv2.imshow('webcam', segmented_img)
-        if cv2.waitKey(1)==32:
-            cv2.imwrite('hand.jpg', segmented_img)
+        draw_points.append(process_seg_image.find_finger_point(segmented_img))
+        if len(draw_points) > draw_length:
+            del(draw_points[0])
+        for i in range(len(draw_points)):
+            cv2.circle(img, draw_points[i], point_radius, (0, 0, 255), -1)
+        cv2.imshow('webcam', img)
+        if cv2.waitKey(1) == 32:
             break
     cv2.destroyAllWindows()
--- segment_hand.py
+++ segment_hand.py
@@ -8,12 +8,12 @@ from matplotlib import pyplot as plt
 
 def segment_hand(histogram, img):
     img_hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-    dst = cv2.calcBackProject([img_hsv], [0, 1], histogram, [0, 180, 0, 256], 1)
+    bp = cv2.calcBackProject([img_hsv], [0, 1], histogram, [0, 180, 0, 256], 1)
 
     disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
-    cv2.filter2D(dst, -1, disc, dst)
+    cv2.filter2D(bp, -1, disc, bp)
 
-    ret, thresh = cv2.threshold(dst, 150, 255, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(bp, 150, 255, cv2.THRESH_BINARY)
 
     thresh = cv2.merge((thresh, thresh, thresh))
 
