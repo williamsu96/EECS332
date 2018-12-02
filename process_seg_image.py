c48c0ac55281c830c8d85f705564c7d6e049c1de
Tim Huang <tim81517lh@gmail.com>
2018-12-02 15:45:50 -0600

    max_contour prototype

--- process_seg_image.py
+++ process_seg_image.py
@@ -1,6 +1,18 @@
 import cv2
 import numpy
 
+def max_contour(contours):
+    # Looks through a list of contours and return an index with the largest number of vertices (longest contour)
+    max_contour_index = 0
+    max_contour_len = 0
+    if len(contours) == 0:
+        raise Exception('Invalid/empty list of contours')
+    else:
+        for i in range(0, len(contours)):
+            if (len(contours[i]) > max_contour_len):
+                max_contour_index = i
+        return max_contour_index
+
 def find_finger_point(frame):
     # takes in a segmented frame (hand-only) and returns a cartesian coordinate (x,y)
 
