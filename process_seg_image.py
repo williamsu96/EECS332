ad7b73a6d44493c44939a515df8963444a38823d
Tim Huang <tim81517lh@gmail.com>
2018-12-02 17:33:00 -0600

    Merge branch 'master' of /Users/tim/Dropbox/Northwestern/Fall 2018/EECS 332/Project/FingerCursor with conflicts.

--- process_seg_image.py
+++ process_seg_image.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy
+from scipy.spatial import distance
 
 def max_contour(contours):
-    # Looks through a list of contours and return an index with the largest number of vertices (longest contour)
+    # Iterates through a list of contours and return an index with the largest number of vertices (longest contour)
     max_contour_index = 0
     max_contour_len = 0
     if len(contours) == 0:
@@ -12,6 +13,22 @@ def max_contour(contours):
             if (len(contours[i]) > max_contour_len):
                 max_contour_index = i
         return max_contour_index
+
+def furthest_vertex(defects, contour, centroid):
+    # Takes in a defect object via cv2.convexityDefect, a contour via cv2.findContours, and the centroid of the contour
+    # Returns the index to the vertex with greatest distance from the centroid
+    max_vertex_dist = 0
+    max_vertex_index = 0
+    for i in range(0, defects.shape[0]):
+        start, _, _, _ = defects[i,0] #only need start vertex index
+        dist = distance.euclidean(centroid, contour[start])
+        if dist > max_vertex_dist:
+            max_vertex_dist = dist
+            max_vertex_index = start
+
+    return max_vertex_index
+
+
 
 def find_finger_point(frame):
     # takes in a segmented frame (hand-only) and returns a cartesian coordinate (x,y)
